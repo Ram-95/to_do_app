eac170ba5161b336f61a4990fe6c9d247cbbd057
Ram Babu <ram.kottapally@gmail.com>
2021-01-18 19:42:48 +0530

    Updated settings

--- to_do_app/settings.py
+++ to_do_app/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['to-do-app-pydj.herokuapp.com']
 
