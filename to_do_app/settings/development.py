eb2301321d0ad9ebe9b23b66838a018459c67227
Ram Babu <ram.kottapally@gmail.com>
2021-07-01 20:59:54 +0530

    Added context_processors to settings

--- to_do_app/settings/development.py
+++ to_do_app/settings/development.py
@@ -87,6 +87,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'to_do.context_processors.get_year',
             ],
         },
     },
