2664f10c23474dd23cb6f8e10feacc83dbd87014
Ram Babu <ram.kottapally@gmail.com>
2021-07-01 20:44:10 +0530

    Changed django_settings_module to production

--- manage.py
+++ manage.py
@@ -5,7 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'to_do_app.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'to_do_app.settings.production')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
